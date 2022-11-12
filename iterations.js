124b518a95975a81ad214460de09201c9723d270
barveenbanu <111632582+barveenbanu@users.noreply.github.com>
2022-11-12 23:10:39 +0530

    Add files via upload

---
+++ iterations.js
@@ -0,0 +1,30 @@
+//Iterations
+
+//iterations for 
+	var arr = [1, 2, 3]
+	for (var i=0; i < arr.length; i++)
+		{
+			console.log(arr[i]);
+		}
+  
+//iterations for of  
+	var arr2 = ["one", "two", "three"]
+	for (var j of arr2)
+		{
+			console.log(j);
+		}
+//iterations for in  
+	var arr3 = ["true", "false"]
+	for (var k in arr3)
+		{
+			console.log(arr3[k]);
+			//console.log(k);
+  
+		}
+
+//iterations forEach
+
+
+	
+
+	
